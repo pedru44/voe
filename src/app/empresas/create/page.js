6de420ff36c7dbae5a57dd5821f65f5ca695c2bc
pedru44 <pedroc@iesb.edu.br>
2024-09-23 21:52:47 -0300

    atualizando

--- src/app/empresas/create/page.js
+++ src/app/empresas/create/page.js
@@ -10,25 +10,44 @@ import { TiArrowBack } from "react-icons/ti";
 
 export default function Page() {
 
+    function salvar(dados) {
+        console.log(dados)
+    }
+
     return (
         <Pagina titulo="Empresas">
-            <Formik>
+            <Formik initialValues={{ nome: '', logo: '' }}
+                onSubmit={values => salvar(values)}
+            >
                 {({
                     values,
                     handleChange,
                     handleSubmit,
                 }) => (
                     <Form>
+
                         <Form.Group className="mb-3" controlId="nome">
                             <Form.Label>Nome</Form.Label>
-                            <Form.Control type="text" name="nome" />
+                            <Form.Control
+                                type="text"
+                                name="nome"
+                                values={values.nome}
+                                onChange={handleChange('nome')}
+                            />
                         </Form.Group>
+
                         <Form.Group className="mb-3" controlId="logo">
                             <Form.Label>Logo</Form.Label>
-                            <Form.Control type="text" name="logo" />
+                            <Form.Control
+                                type="text"
+                                name="logo"
+                                values={values.logo}
+                                onChange={handleChange('logo')}
+                            />
                         </Form.Group>
+
                         <div className="text-center">
-                            <Button variant="success">
+                            <Button onClick={handleSubmit} variant="success">
                                 <FaCheck /> Salvar
                             </Button>
                             <Link href="/empresas"
@@ -36,6 +55,7 @@ export default function Page() {
                                 <TiArrowBack /> Voltar
                             </Link>
                         </div>
+
                     </Form>
                 )}
             </Formik>
